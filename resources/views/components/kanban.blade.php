774b9303acb66c1e0ad8a249c6cbacb2efe2bdcf
Alexander Nikushkin <nikushkin@mail.ru>
2026-01-25 11:46:33 +0500

    chore: style

--- resources/views/components/kanban.blade.php
+++ resources/views/components/kanban.blade.php
@@ -36,7 +36,8 @@
             @if($components->isNotEmpty())
                 <div class="w-full overflow-hidden">
                     <div
-                        class="w-full overflow-x-auto [scrollbar-width:thin] [overflow-style:auto] [-ms-overflow-style:auto]"
+                        class="w-full overflow-x-auto"
+                        style="scrollbar-width: thin; -webkit-overflow-scrolling: touch; overflow-x: scroll;"
                         x-data="kanbanBoardScroll"
                     >
                         <div class="flex gap-2 select-none items-start min-w-max">
